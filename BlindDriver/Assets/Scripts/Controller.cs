bb4a3cb681e155feb7a722fbf857cae4b8c1005e
Unknown <martijn.w3@gmail.com>
2018-09-10 13:47:18 +0200

    Map met onder andere het level

---
+++ BlindDriver/Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public float movementSpeed;
+	// Use this for initialization
+	void Start () {
+        movementSpeed = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * movementSpeed, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * - Time.deltaTime * movementSpeed, Space.Self);
+        }
+    }
+}
---
+++ BlindDriver/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        movementSpeed = 15;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.z += movementSpeed;
+            this.transform.position = position;
+        }
+
+    }
+}
